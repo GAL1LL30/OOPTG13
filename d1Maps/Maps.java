3e35539b52ffd4ac62c8b96da3e8337ec5e9e97d
GAL1LL30 <73478692+GAL1LL30@users.noreply.github.com>
2023-12-04 08:22:27 +0000

    Beispielcode Maps(nicht fertig)

---
+++ d1Maps/Maps.java
@@ -0,0 +1,18 @@
+package d1Maps;
+import java.util.HashMap;
+import java.util.Map;
+
+class Maps {
+    public static void main(String[] args) {
+        
+        HashMap map = new HashMap();
+
+        Schuler s1 = new Schuler("Hoch","Lennard",1);
+        Schuler s2 = new Schuler("Feuerstein", "Niklas", 2);
+        Schuler s3 = new Schuler("Raible", "Florian", 3);
+        Schuler s4 = new Schuler("Frank", "Elias", 1);
+
+        
+    }
+    
+}
---
+++ d1Maps/Schuler.java
@@ -0,0 +1,14 @@
+package d1Maps;
+
+public class Schuler {
+    
+    String name,vorname;
+    int ParkplatzNr;
+    
+    public Schuler(String name, String vorname, int ParkplatzNr)
+        {
+            this.name = name;
+            this.vorname = vorname;
+            this.ParkplatzNr = ParkplatzNr;
+        }
+}
