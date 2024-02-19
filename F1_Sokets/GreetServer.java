a9e6d39cea099914222b39a7aa04ca3de2575f92
GAL1LL30 <73478692+GAL1LL30@users.noreply.github.com>
2024-02-19 08:25:43 +0000

    Sockets

---
+++ F1_Sokets/GreetServer.java
@@ -0,0 +1,38 @@
+package F1_Sokets;
+
+import java.net.*;
+import java.io.*;
+
+
+public class GreetServer {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void start(int port){
+        serverSocket = new ServerSocket(port);
+        clientSocket = new serverSocket.accept();
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String greeting = in.readLine();
+        if("hello server".equals(greeting)){
+            out.println("hello client");
+        }
+        else{
+            out.println("unrecogmised greeting");
+        }
+    }
+
+    public void stop(){
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+
+    public static void main(String[] args) {
+        GreetServer server = new GreetServer();
+        server.start(6666);
+    }
+}
